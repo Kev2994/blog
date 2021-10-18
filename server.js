d25f1e7f3314b1af0f8fc20a3187fbaad7599b49
Kevin Virani <kevinvirani5@gmail.com>
2021-10-18 15:29:46 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog')
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+app.use('/articles', articleRouter);
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find();
+    res.render('articles/index', {articles:articles})
+})
+app.listen(5000);
